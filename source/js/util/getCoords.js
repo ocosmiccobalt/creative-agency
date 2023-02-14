871bbb12f1eeb3f39d3496c33b0957701c6b315c
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-02-14 21:08:55 +0300

    Add getCoords.js (util)

---
+++ source/js/util/getCoords.js
@@ -0,0 +1,12 @@
+const getCoords = (elem) => {
+  const box = elem.getBoundingClientRect();
+
+  return {
+    top: box.top + window.pageYOffset,
+    right: box.right + window.pageXOffset,
+    bottom: box.bottom + window.pageYOffset,
+    left: box.left + window.pageXOffset
+  };
+};
+
+export default getCoords;
