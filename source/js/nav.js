09715363e5bcb7921da12b135cd46ac9014e6bf6
Olga Bazhenova <ocosmiccobalt@gmail.com>
2023-02-14 21:17:31 +0300

    Close nav on link click when it is on top of linked section

--- source/js/nav.js
+++ source/js/nav.js
@@ -1,3 +1,5 @@
+import getCoords from './util/getCoords.js';
+
 class Nav {
   constructor(navElem, buttonClass, menuClass) {
     this.navElem = navElem;
@@ -13,11 +15,22 @@ class Nav {
     if (this.button !== null && this.menu !== null) {
       // show button
       this.button.classList.remove(this.buttonNoJSClass);
-      // close menu
+      this.button.setAttribute(`aria-expanded`, false);
+      // hide menu
       this.menu.classList.remove(this.menuNoJSClass);
 
-      this.button.setAttribute(`aria-expanded`, false);
       this.button.addEventListener(`click`, this.onButtonClick.bind(this));
+
+      this.innerLinks = this.menu.querySelectorAll(`a[href^='#']`);
+
+      this.innerLinks.forEach((link) => {
+        const href = link.getAttribute(`href`);
+        link.pageFragment = (href.length > 1) ? document.querySelector(href) : null;
+
+        if (link.pageFragment !== null) {
+          link.addEventListener(`click`, this.onLinkClick.bind(this));
+        }
+      });
     }
   }
 
@@ -28,6 +41,18 @@ class Nav {
     target.setAttribute(`aria-expanded`, !expanded);
     target.classList.toggle(this.buttonOpenClass);
     this.menu.classList.toggle(this.menuOpenClass);
+  }
+
+  onLinkClick(evt) {
+    const target = evt.currentTarget;
+    const linkedFragmentCoords = getCoords(target.pageFragment);
+    const menuCoords = getCoords(this.menu);
+
+    if (menuCoords.bottom > linkedFragmentCoords.top) {
+      this.button.setAttribute(`aria-expanded`, false);
+      this.button.classList.remove(this.buttonOpenClass);
+      this.menu.classList.remove(this.menuOpenClass);
+    }
   }
 }
 
